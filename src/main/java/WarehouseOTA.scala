a8418927810b185ab8c7da73b4267cde0aad007d
sraspin <Sam.Raspin@netbuilder.com>
2015-11-05 12:22:15 +0000

    stuff

--- src/main/java/WarehouseOTA.scala
+++ src/main/java/WarehouseOTA.scala
@@ -19,18 +19,38 @@ import scalafx.scene.paint.Color
 import scalafx.scene.shape.Circle
 import scalafx.scene.layout.BorderPane
 import scalafx.scene.control.Label
+import scalafx.scene.layout.GridPane
 
 
 object WarehouseOTA extends JFXApp{
   stage = new PrimaryStage {
+    title = "Raspin LogIn"
+    stage width = 300
+    stage height = 400
     scene = new Scene {
       root = new BorderPane {
         padding = Insets(25)
-        center = new Label("Howdy Doodly")
+        content = new HBox {
+         children = Seq(
+           new GridPane {
+             add(new Label("asdfsdf"), 1, 1)
+             add(new Label("bsdfsdf"), 1, 2)
+             }
+          )
+        }
+        //right = new Label("Howdy Doodly")
         val data = new Database
         val a = new Employee
-        val s = a loggingIn(data connection)
-        center = new Label(s)
+        
+        def newmeth (n:Int) {
+          if(n <= 1){
+            val s = a loggingIn(data connection)
+            //left = new Label(s)
+          } else {
+            newmeth(n-1)
+          }
+        }
+        newmeth(2)
       }
     }
   }

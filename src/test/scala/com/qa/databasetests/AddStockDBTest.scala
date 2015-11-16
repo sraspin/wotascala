66a763d19ddaa223fe210529927b2348196f1a7c
sraspin <Sam.Raspin@netbuilder.com>
2015-11-16 17:55:15 +0000

    added test for addStockDB in comments

--- src/test/scala/com/qa/databasetests/AddStockDBTest.scala
+++ src/test/scala/com/qa/databasetests/AddStockDBTest.scala
@@ -3,12 +3,14 @@ package com.qa.databasetests
 import com.qa.database.{AddStockDB,Database}
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.sql.ResultSet
 
 /**
  * @author sraspin
  */
 class AddStockDBTest extends TestBase{
-  /*
+  /* DUE TO THE TEST DEPENDING ON THE EXACT SECOND IT IS CALLED THIS TEST ONLY PASSES 90% OF THE TIME
+   * 
   def testCreateOrder{
     "The method createOrder" should "create an order consisting only of a purchase order ID and status" in{
       val addStockDB = new AddStockDB
@@ -23,8 +25,17 @@ class AddStockDBTest extends TestBase{
       
       val resultSet = database.connect().createStatement.executeQuery("SELECT * FROM purchaseorder WHERE idPOrder = '" + datei + "'")
       
-      def getData(rs: ResultSet); String
+      def getData(rs: ResultSet): String = {
+        if(rs next){
+          val result = rs.getString(2)
+          result
+        } else{
+          null
+        }
+      }
+      assert(getData(resultSet) == "Pending")
     }
   }
+  testCreateOrder
   */
 }
